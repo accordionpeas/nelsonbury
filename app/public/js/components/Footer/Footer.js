89331c04fcc55c4a9e39d957ace6b2f747852c97
Michael Nelson <michael.nelson@beamly.com>
2019-12-04 21:59:17 +0000

    Add footer image

--- app/public/js/components/Footer/Footer.js
+++ app/public/js/components/Footer/Footer.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import resolveAsset from '../../utils/resolve-asset';
 
-const Footer = () => (
-  <footer className="footer" />
+const Footer = ({
+  assetManifest,
+}) => (
+  <footer className="footer">
+    <img
+      className="footer__image"
+      src={`/assets/${resolveAsset({ filename: 'footer.png', assetManifest })}`}
+    />
+  </footer>
 );
+
+Footer.propTypes = {
+  assetManifest: PropTypes.objectOf(PropTypes.string),
+};
 
 export default Footer;
